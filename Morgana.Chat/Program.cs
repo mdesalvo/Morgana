4910d3ad3b1a9782b856f3a034620494e2cd0863
mdesalvo <floating_point_9@msn.com>
2025-12-06 18:02:36 +0100

    Add Morgana.Chat project

---
+++ Morgana.Chat/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Morgana.Chat.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+// Configurazione API endpoint
+builder.Services.AddScoped(sp => new HttpClient 
+{ 
+    BaseAddress = new Uri(builder.Configuration["Morgana:BaseUrl"] ?? "https://localhost:5001") 
+});
+
+// SignalR client service
+builder.Services.AddScoped<MorganaSignalRService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
