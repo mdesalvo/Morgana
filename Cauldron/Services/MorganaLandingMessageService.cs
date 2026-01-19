473911bf3a0c319e96ff182d31bf3c7a7e04f3f7
mdesalvo <floating_point_9@msn.com>
2026-01-19 20:38:14 +0100

    Added support for dynamic resuming messages

--- Cauldron/Services/MorganaLandingMessageService.cs
+++ Cauldron/Services/MorganaLandingMessageService.cs
@@ -3,19 +3,30 @@
 public class MorganaLandingMessageService
 {
     private readonly string[] landingMessages;
+    private readonly string[] resumingMessages;
     private readonly Random random = new Random();
 
     public MorganaLandingMessageService(IConfiguration configuration)
     {
         landingMessages = configuration.GetSection("Morgana:LandingMessages").Get<string[]>()
                             ?? ["✨ A pinch of magic and I'll be ready... ✨"];
+        resumingMessages = configuration.GetSection("Morgana:ResumingMessages").Get<string[]>()
+                           ?? ["🔮 The circle is open once more. We resume where the spell paused. 🔮"];
     }
 
-    public string GetRandomMessage()
+    public string GetRandomLandingMessage()
     {
         lock (random)
         {
             return landingMessages[random.Next(landingMessages.Length)];
+        }
+    }
+    
+    public string GetRandomResumingMessage()
+    {
+        lock (random)
+        {
+            return resumingMessages[random.Next(resumingMessages.Length)];
         }
     }
 }
